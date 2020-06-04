a55dd6d0e717125704f0446303a644ca8dd67a71
tetiana <tetianayaremko@gmail.com>
2020-06-04 14:10:56 +0200

    gets randomBook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import WarningSign from "./components/WarningSign";
 import MyBadge from "./components/MyBadge";
+import books from "./data/fantasy.json"
+import SingleBook from "./components/SingleBook";
 
 class App extends Component {
+
     render() {
+        const randomBook = books[Math.floor(Math.random() * books.length)]
+        console.log(randomBook)
         return (
             <>
                 <WarningSign text="This is a WorningSign component"/>
                 <MyBadge text={"This is MyBadge component"} variant={"secondary"}/>
+                <SingleBook bookObject = {randomBook}/>
             </>
         );
     }
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 
 class SingleBook extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
             <div>
-                
+
             </div>
         );
     }
